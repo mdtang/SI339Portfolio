4c0c5a2a5b806b0e11c1b764f39e62b76c86e886
Mingda Tang <mdtang@umich.edu>
2018-03-23 17:11:57 -0400

    Added text and mouseover/mouseleave animations for front page panels

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-    $('#frontpage').fadeIn();
+$(document).ready(function() {
+  $('.frontpageimages').on('focus mouseover', function() {
+    $(this).prev().css('display', 'block');
+  });
+  $('.frontpageimages').on('blur mouseleave', function() {
+    $(this).prev().css('display', 'none');
+  });
 });
